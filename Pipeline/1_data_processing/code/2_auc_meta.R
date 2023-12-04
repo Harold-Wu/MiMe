caa41a9a6b2116afa59ee9796a8abb5261f8803e
Harold Wu <hwuwu95@gmail.com>
2023-12-04 10:09:44 -0500

    Add files via upload

---
+++ Pipeline/1_data_processing/code/2_auc_meta.R
@@ -0,0 +1,79 @@
+#!/usr/bin/env Rscript
+#SBATCH --job-name=meta_pred
+#SBATCH --time=4:00:00
+#SBATCH --mem=4G
+#SBATCH --array=1-243
+#SBATCH --output=../logs/%x_%A_%a
+#SBATCH --constraint=[intel18|intel16]
+# Usage:
+#
+#  sbatch 2_auc_meta.R
+
+jobID <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID", "1"))
+omic <- "meta"
+
+library(lme4)
+library(MASS)
+library(dplyr)
+library(pROC)
+library(data.table)
+
+load(paste0("/Data/", omic, "_QCed_data.RData"))
+data <- meta_data
+
+data_t0 <- data[which(data$timepoint == 2),c(-1:-8)]
+tmp <- colMeans(data_t0)
+
+ID <- data$subjectID
+age <- data$age_mos
+timepoint <- data$timepoint
+sex <- as.factor(data$sex)
+sample <- data$sampleID
+race <- data$race
+Y <- data[,-c(1,2,3,4,5,6,7,8)]
+
+start <- 10*(jobID-1)+1
+end <- ifelse(jobID==243, 2422, 10*jobID)
+
+repeats <- 20
+
+TASKS1 <- matrix(nrow = 2422, ncol = repeats+1)
+TASKS1[,1] <- colnames(data)[9:2430]
+colnames(TASKS1) <- c("rowname", 1:repeats)
+rownames(TASKS1) <- colnames(data)[9:2430]
+TASK1 <- TASKS1[start:end,]
+
+TASKS2 <- matrix(nrow = 2422, ncol = repeats+1)
+TASKS2[,1] <- colnames(data)[9:2430]
+colnames(TASKS2) <- c("rowname", 1:repeats)
+rownames(TASKS2) <- colnames(data)[9:2430]
+TASK2 <- TASKS2[start:end,]
+
+for (i in 1:nrow(TASK1)) {
+  for (r in 1:repeats) {
+    tryCatch({
+    y <- Y[,start-1+i]
+    full_table <- data.frame("y" = y,
+                               "sample" = sample,
+                               "ID" = ID,
+                               "age" = age,
+                               "sex" = factor(sex),
+                               "race" = factor(race))
+    full_table$y <- as.numeric(full_table$y != 0)
+    heads <- as.data.frame(full_table %>% group_by(ID) %>% arrange(desc(age)) %>% slice_head(n=1))
+    tails <- as.data.frame(full_table %>% group_by(ID) %>% arrange(desc(age)) %>% slice_tail(n=1))
+    remaining <- full_table[-which(full_table$sample %in% heads$sample | full_table$sample %in% tails$sample),]
+    out_table <- as.data.frame(remaining %>% group_by(ID) %>% slice_sample(n=1))
+    fit_table <- full_table[-which(full_table$sample %in% out_table$sample),]
+    fm1 <- glmer(y ~ age+sex+race+(age|ID), data = fit_table, family = "binomial", control = glmerControl(optimizer = "bobyqa"))
+    TASK1[i,r+1] <- auc(out_table$y, predict(fm1, newdata = out_table, type = "response"))
+    fm2 <- glm(y ~ age+sex+race, data = fit_table, family = "binomial")
+    TASK2[i,r+1] <- auc(out_table$y, predict(fm2, newdata = out_table, type = "response"))
+  }, error=function(e){cat("ERROR :",conditionMessage(e), "\n")})
+  }
+  fwrite(TASK1[i,], file='../output/meta_auc_test.csv', append=TRUE)
+  fwrite(TASK2[i,], file='../output/meta_noran_auc_test.csv', append=TRUE)
+  print(i)
+}
+
+quit(save='no')
---
+++ Pipeline/2_model/code/4_make_Figure_S2.R
@@ -0,0 +1,77 @@
+# Figure S2
+library(ggplot2)
+library(reshape2)
+
+load("../output/micro_pop_expected.RData")
+load("../output/meta_pop_expected.RData")
+
+load("Data/micro_filter_logit_data.RData")
+load("Data/meta_filter_logit_data.RData")
+
+PoP_meta <- data.frame("name" = rownames(meta_prob),
+             "t2_real" = NA,
+             "t2_pred" = meta_prob[,3],
+             "t4_real" = NA,
+             "t4_pred" = meta_prob[,7],
+             "t6_real" = NA,
+             "t6_pred" = meta_prob[,11],
+             "t9_real" = NA,
+             "t9_pred" = meta_prob[,17],
+             "t12_real" = NA,
+             "t12_pred" = meta_prob[,23])
+meta_01 <- meta_data[,-1:-8] != 0
+meta_01 <- cbind("time" = meta_data[,4], meta_01)
+for (i in 2:ncol(meta_01)) {
+  PoP_meta[which(PoP_meta$name == colnames(meta_01)[i]),2] <- aggregate(meta_01[,i], list(meta_01[,1]), FUN=mean)[1,2]
+  PoP_meta[which(PoP_meta$name == colnames(meta_01)[i]),4] <- aggregate(meta_01[,i], list(meta_01[,1]), FUN=mean)[2,2]
+  PoP_meta[which(PoP_meta$name == colnames(meta_01)[i]),6] <- aggregate(meta_01[,i], list(meta_01[,1]), FUN=mean)[3,2]
+  PoP_meta[which(PoP_meta$name == colnames(meta_01)[i]),8] <- aggregate(meta_01[,i], list(meta_01[,1]), FUN=mean)[4,2]
+  PoP_meta[which(PoP_meta$name == colnames(meta_01)[i]),10] <- aggregate(meta_01[,i], list(meta_01[,1]), FUN=mean)[5,2]
+}
+
+PoP_micro <- data.frame("name" = rownames(micro_prob),
+              "t2_real" = NA,
+              "t2_pred" = micro_prob[,3],
+              "t4_real" = NA,
+              "t4_pred" = micro_prob[,7],
+              "t6_real" = NA,
+              "t6_pred" = micro_prob[,11],
+              "t9_real" = NA,
+              "t9_pred" = micro_prob[,17],
+              "t12_real" = NA,
+              "t12_pred" = micro_prob[,23])
+micro_01 <- micro_data[,-1:-8] != 0
+micro_01 <- cbind("time" = micro_data[,4], micro_01)
+for (i in 2:ncol(micro_01)) {
+  PoP_micro[which(PoP_micro$name == colnames(micro_01)[i]),2] <- aggregate(micro_01[,i], list(micro_01[,1]), FUN=mean)[1,2]
+  PoP_micro[which(PoP_micro$name == colnames(micro_01)[i]),4] <- aggregate(micro_01[,i], list(micro_01[,1]), FUN=mean)[2,2]
+  PoP_micro[which(PoP_micro$name == colnames(micro_01)[i]),6] <- aggregate(micro_01[,i], list(micro_01[,1]), FUN=mean)[3,2]
+  PoP_micro[which(PoP_micro$name == colnames(micro_01)[i]),8] <- aggregate(micro_01[,i], list(micro_01[,1]), FUN=mean)[4,2]
+  PoP_micro[which(PoP_micro$name == colnames(micro_01)[i]),10] <- aggregate(micro_01[,i], list(micro_01[,1]), FUN=mean)[5,2]
+}
+
+
+tmp1 <- melt(PoP_meta[,c(1,2,4,6,8,10)])
+tmp2 <- melt(PoP_meta[,c(1,3,5,7,9,11)])
+meta_long <- cbind(tmp1, tmp2)[,c(1,2,3,6)]
+colnames(meta_long) <- c("name", "time", "observed", "predicted")
+meta_long$time <- as.factor(rep(c(2,4,6,9,12), each = 1115))
+meta_long[,3:4] <- 1-meta_long[,3:4]
+
+s2a <- ggplot(meta_long, aes(x = observed, y = predicted, color = time))+
+  geom_point(size = 0.5)+
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", size = 0.3, color = "darkgray")+
+  labs(title = "Metabolome", x = "Observed proportion of zeros", y = "Expected proportion of zeros", color = "Time (months)")
+
+tmp1 <- melt(PoP_micro[,c(1,2,4,6,8,10)])
+tmp2 <- melt(PoP_micro[,c(1,3,5,7,9,11)])
+micro_long <- cbind(tmp1, tmp2)[,c(1,2,3,6)]
+colnames(micro_long) <- c("name", "time", "observed", "predicted")
+micro_long$time <- as.factor(rep(c(2,4,6,9,12), each = 84))
+micro_long[,3:4] <- 1-micro_long[,3:4]
+
+s2b <- ggplot(micro_long, aes(x = observed, y = predicted, color = time))+
+  geom_point(size = 0.5)+
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", size = 0.3, color = "darkgray")+
+  labs(title = "Microbiome", x = "Observed proportion of zeros", y = "Expected proportion of zeros", color = "Time (months)")
+
